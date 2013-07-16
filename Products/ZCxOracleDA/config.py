a191742dbe5df606d6f19e4e37b94da68d7f2aa9
Cleber J Santos <cleber@cleberjsantos.com.br>
2013-07-16 08:02:22 -0300

    Fix typo

--- Products/ZCxOracleDA/config.py
+++ Products/ZCxOracleDA/config.py
@@ -7,7 +7,7 @@ import os
 # of sessions by 1, but, we can change this values setting
 # with environ variable.
 
-CX_POOL_SESSION_MIN = os.environ.pop('CX_POOL_SESSION_MIN', 5)
-CX_POOL_SESSION_MAX = os.environ.pop('CX_POOL_SESSION_MAX', 10)
-CX_POOL_SESSION_INCREMENT = os.environ.pop('CX_POOL_SESSION_INCREMENT', 1)
-CX_POOL_CONNECT_TIMEOUT = os.environ.pop('CX_POOL_CONNECT_TIMEOUT', 50)
+CX_POOL_SESSION_MIN = int(os.environ.pop('CX_POOL_SESSION_MIN', 5))
+CX_POOL_SESSION_MAX = int(os.environ.pop('CX_POOL_SESSION_MAX', 10))
+CX_POOL_SESSION_INCREMENT = int(os.environ.pop('CX_POOL_SESSION_INCREMENT', 1))
+CX_POOL_CONNECT_TIMEOUT = int(os.environ.pop('CX_POOL_CONNECT_TIMEOUT', 50))
